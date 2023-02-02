3fdbdcc2c79ab587987f6efbe53dfccccb53b794
Vinay <vinu031995@gmail.com>
2023-02-02 19:30:13 +0530

    fixed the logic error of getting always tie

--- script.js
+++ script.js
@@ -7,12 +7,23 @@ function getComputerChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-if (playerSelection==computerSelection) {
-  return console.log("You win");
-}
-else {return console.log("You lose");}
+  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return console.log(`You Tie! ${playerSelection} tie with ${computerSelection}`);
+  }
+  else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
+    return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+  if (playerSelection.toLowerCase()=="paper"&& computerSelection.toLowerCase()=="rock") {
+    return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+ if (playerSelection.toLowerCase()=="scissors"&& computerSelection.toLowerCase()=="paper") {
+    return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+  else{
+    return console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
 
 }
 const computerSelection = getComputerChoice();
 const playerSelection = prompt("Enter your option: ");
-playRound(playerSelection,computerSelection);
+playRound(playerSelection, computerSelection);
