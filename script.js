3fd1e04e5ffde2b2466c68380aa59a5898e4153b
Vinay <vinu031995@gmail.com>
2023-02-02 19:59:07 +0530

    added game rounds

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 function getComputerChoice() {
   const arr = ["Rock", "Paper", "Scissors"];
   let i = Math.floor(Math.random() * 3);
-  console.log(arr[i]);
   return arr[i];
 };
+function game() {
+  for (let index = 0; index < 5; index++) {
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    const playerSelection = prompt("Enter your option: ");
+    playRound(playerSelection, computerSelection);
+  }
+}
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
@@ -13,17 +20,15 @@ function playRound(playerSelection, computerSelection) {
   else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
     return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
-  if (playerSelection.toLowerCase()=="paper"&& computerSelection.toLowerCase()=="rock") {
+  if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
     return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
- if (playerSelection.toLowerCase()=="scissors"&& computerSelection.toLowerCase()=="paper") {
+  if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
     return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
-  else{
+  else {
     return console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
   }
 
 }
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Enter your option: ");
-playRound(playerSelection, computerSelection);
+game();
