0259ea198834df43629c7d5e03003dbffcb3c8e4
Vinay <vinu031995@gmail.com>
2023-02-16 23:58:08 +0530

    added the working ui

--- script.js
+++ script.js
@@ -6,42 +6,107 @@ function getComputerChoice() {
   let i = Math.floor(Math.random() * 3);
   return arr[i];
 };
-//game function
-function game() {
-  for (let index = 0; index < 5; index++) {
-    const computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    const playerSelection = prompt("Enter your option: ");
-    playRound(playerSelection, computerSelection);
-  console.log(`The player score is ${player}`);
-  console.log(`The cpu score is ${cpu}`);
-  }
-(player>cpu)?console.log("player wins"):console.log("cpu wins");    
+
+const div = document.querySelector('div');
+const pscore= document.querySelector('.pscore');
+const cpuscore= document.querySelector('.cpuscore');
+const result= document.querySelector('.result');
+
+const Rock = document.querySelector('.rock');
+const Paper= document.querySelector('.paper');
+const Scissors= document.querySelector('.sci');
+
+Rock.addEventListener('click',()=>{
+    
+    let computerSelection = getComputerChoice();
+    playRound(Rock.textContent,computerSelection)
+    cpuscore.textContent = `Cpu : ${cpu}`;
+    pscore.textContent = `Player : ${player}`;
+if(player===5){
+if(player>cpu){
+    result.textContent = "player wins";
+}else{
+    result.textContent = "cpu wins";
+} 
 }
+});
+
+Paper.addEventListener('click',()=>{
+    
+    let computerSelection = getComputerChoice();
+    playRound(Paper.textContent,computerSelection)
+    cpuscore.textContent = `Cpu : ${cpu}`;
+    pscore.textContent = `Player : ${player}`;
+if(player===5){
+if(player>cpu){
+    result.textContent = "player wins";
+}else{
+    result.textContent = "cpu wins";
+} 
+}
+});
+
+Scissors.addEventListener('click',()=>{
+    
+    let computerSelection = getComputerChoice();
+    playRound(Scissors.textContent,computerSelection)
+    cpuscore.textContent = `Cpu : ${cpu}`;
+    pscore.textContent = `Player : ${player}`;
+if(player===5){
+if(player>cpu){
+    result.textContent = "player wins";
+}else{
+    result.textContent = "cpu wins";
+} 
+}
+});
+
+
+
+//game function
+//function game() {
+  //for (let index = 0; index < 5; index++) {
+    //console.log(computerSelection);
+    //const playerSelection = prompt("Enter your option: ");
+   // playRound(playerSelection, computerSelection);
+  //console.log(`The player score is ${player}`);
+  //console.log(`The cpu score is ${cpu}`);
+ //}
+//(player>cpu)?console.log("player wins"):console.log("cpu wins");    
+//}
+
 
 
 //function for each rounds
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-    return;
+    //return;
+      div.textContent = `You Tie! ${playerSelection} tie with ${computerSelection}`;
     // return console.log(`You Tie! ${playerSelection} tie with ${computerSelection}`);
   }
   else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
-    return player+=1;
-    // return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    //return player+=1;
+    player+=1;
+      div.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+     //return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
   else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
-     return player+=1;
-    // return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+     //return player+=1;
+     player+=1;
+      div.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+     //return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
   else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
-   return player+=1;
-    // return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+   //return player+=1;
+   player+=1;
+      div.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+     //return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   }
   else {
-return cpu+=1;
-    // return console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+//return cpu+=1;
+cpu+=1;
+      div.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+     //return console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
   }
 
 }
-game();
